59d38b1b51c9873ff76d81e6c46b2f33cbb88347
Kevin <kevin@KevindeiMac.local>
2016-12-20 13:02:01 +0800

    接口配置文件配置成功

---
+++ mockConfig.js
@@ -0,0 +1,18 @@
+"use strict";
+
+let mock = [
+	{
+		type: 'post',
+		url: '/api/login',
+		status: 200,
+		resData: {
+    		userName: '11',
+    		password: '2222'
+        }
+	}
+]
+
+
+
+
+exports.mock = mock;
--- server.js
+++ server.js
@@ -12,8 +12,8 @@ var compiler = webpack(config);
 app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use(devMiddleware(compiler, {
-  publicPath: config.output.publicPath,
-  historyApiFallback: true,
+    publicPath: config.output.publicPath,
+    historyApiFallback: true,
 }));
 
 app.use(hotMiddleware(compiler));
@@ -23,26 +23,57 @@ const login = require('./server/login.js');
 
 app.use(express.static(__dirname))
 
-app.all('*',function(req,res,next){
+app.all('*', function(req, res, next) {
 
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
-    res.header("X-Powered-By",' 3.2.1')
+    res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
+    res.header("X-Powered-By", ' 3.2.1')
     res.header("Content-Type", "text/html");
     next();
 })
 
-app.get('*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'src/index.html'));
+app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'src/index.html'));
 });
 
-app.post('/api/login', login.login);
 
-app.listen(5000, function (err) {
-  if (err) {
-    return console.error(err);
-  }
+var mockConfig = require('./mockConfig.js');
 
-  console.log('Listening at http://localhost:5000/');
+console.log(mockConfig.length)
+
+
+for (var i = 0; i < mockConfig.mock.length; i++) {
+
+    if (mockConfig.mock[i].type == 'post') {
+        console.log('建立成功')
+        console.log(mockConfig.mock[i].url)
+
+        var resData = mockConfig.mock[i].resData
+        app.post(mockConfig.mock[i].url, function(req, res, next) {
+            console.log(mockConfig.mock[i])
+            res.json({
+                status: 200,
+                data: resData
+            });
+        })
+    }
+}
+
+// app.post('/api/login', function(req, res, next) {
+//     console.log(req)
+//     res.json({
+//         status: 200,
+//         data: 111
+//     });
+// })
+
+// app.post('/api/login', login.login);
+
+app.listen(5000, function(err) {
+    if (err) {
+        return console.error(err);
+    }
+
+    console.log('Listening at http://localhost:5000/');
 });
